1f4a35c307580c49c0c13f5d9ce660f4e9eadb55
Cojocariu Josua <cojocariujosu@yahoo.com>
2023-03-11 13:17:45 +0200

    first exercise course 6

---
+++ src/Person.java
@@ -0,0 +1,30 @@
+
+public class Person {
+    private String name;
+    private int age;
+    private boolean married;
+
+
+    public Person(String name, int age, boolean married) {
+        this.name = name;
+        this.age = age;
+        this.married = married;
+        if(married == true) {
+        System.out.println(name + " has " + age + " years old and it's married"); }
+        else {
+            System.out.println(name + " has " + age + " years old and it's not married"); }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isMarried() {
+        return isMarried();
+    }
+}
+
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Person p1 = new Person("Emilia",55, true);
+        Person p2 = new Person("Costica", 31,false);
+        Person p3 = new Person("Elvira", 22,true);
+
+
+    }
+}
